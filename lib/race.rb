535ffaffa8c497f7a10f88267cf7ba2499b73847
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-08 13:26:08 -0700

    Feat: Initialize and register_candidate! methods of Race class

--- lib/race.rb
+++ lib/race.rb
@@ -1,2 +1,16 @@
 class Race
+    attr_reader :office,
+                :candidates
+
+    def initialize(office)
+        @office = office
+        @candidates = []
+    end
+
+    def register_candidate!(data)
+        candidate = Candidate.new(data)
+
+        candidates << candidate
+        candidate
+    end
 end

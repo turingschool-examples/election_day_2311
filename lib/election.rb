d75d83d95a0caa82d0dcb15b9a4532feaa1f55bf
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-08 14:05:43 -0700

    Feat: create initialize, add_race, candidates and vote_counts methods of Election class

--- lib/election.rb
+++ lib/election.rb
@@ -1,2 +1,30 @@
 class Election
+    attr_reader :year,
+                :races,
+                :election_candidates
+
+    def initialize(year)
+        @year = year
+        @races = []
+        @election_candidates = []
+    end
+
+    def add_race(race)
+        @races << race
+        @races
+    end
+
+    def candidates
+        @races.map {|race| @election_candidates << race.candidates}
+        @election_candidates = @election_candidates.flatten!
+    end
+
+    def vote_counts
+        vote_count = {}
+        candidates
+        @election_candidates.each do |candidate|
+            vote_count[candidate.name] = candidate.votes
+        end
+        vote_count
+    end
 end
